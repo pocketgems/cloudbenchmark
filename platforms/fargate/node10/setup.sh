79732901b8d8dae311a095fabf3e7b5104644962
David Underhill <david@pocketgems.com>
2019-11-07 16:47:34 -0800

    comments for aws setup script

--- platforms/fargate/node10/setup.sh
+++ platforms/fargate/node10/setup.sh
@@ -3,6 +3,7 @@
 #  2) setup AWS service account with full DynamoDB access; make creds.json with
 #     its access id/key and the region we want to test in
 
+# create the DynamoDB tables we need
 tableNames=('JSCounter' 'BigJsonHolder' 'OneInt')
 for idx in `seq 2 2`; do
     if [ $idx -eq 2 ]; then
@@ -14,9 +15,12 @@ for idx in `seq 2 2`; do
     aws dynamodb create-table --table-name ${tableNames[$idx]} --key-schema AttributeName=id,KeyType=HASH --attribute-definitions AttributeName=id,AttributeType=${type} --billing-mode PAY_PER_REQUEST
 done
 
+# populate the OneInt table
 for idx in `seq 0 9999`; do
     if [ $idx -ne 0 ]; then
         echo "   $idx done"
     fi
     aws dynamodb put-item --table-name OneInt --item "{\"id\":{\"N\":\"${idx}\"}}"
 done
+
+# elasticache and fargate setup are manual for now
